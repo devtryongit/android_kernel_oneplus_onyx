dbe28f38c0e84784e85ba351298a50a2325bb59e
Alex Shi <alex.shi@intel.com>
2012-05-14 14:15:31 -0700

    net: replace percpu_xxx funcs with this_cpu_xxx or __this_cpu_xxx
    percpu_xxx funcs are duplicated with this_cpu_xxx funcs, so replace
    them for further code clean up.
    
    And in preempt safe scenario, __this_cpu_xxx funcs may has a bit
    better performance since __this_cpu_xxx has no redundant
    preempt_enable/preempt_disable on some architectures.
    
    Signed-off-by: Alex Shi <alex.shi@intel.com>
    Acked-by: Eric Dumazet <eric.dumazet@gmail.com>
    Acked-by: David S. Miller <davem@davemloft.net>
    Cc: Patrick McHardy <kaber@trash.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- net/socket.c
+++ net/socket.c
@@ -489,7 +489,7 @@ static struct socket *sock_alloc(void)
 	inode->i_uid = current_fsuid();
 	inode->i_gid = current_fsgid();
 
-	percpu_add(sockets_in_use, 1);
+	this_cpu_add(sockets_in_use, 1);
 	return sock;
 }
 
@@ -535,7 +535,7 @@ void sock_release(struct socket *sock)
 	if (test_bit(SOCK_EXTERNALLY_ALLOCATED, &sock->flags))
 		return;
 
-	percpu_sub(sockets_in_use, 1);
+	this_cpu_sub(sockets_in_use, 1);
 	if (!sock->file) {
 		iput(SOCK_INODE(sock));
 		return;
