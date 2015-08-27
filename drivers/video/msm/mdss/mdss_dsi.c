a9acd41b416b48d5952d4ad6d7261271aa25ddf9
andip71 <andreasp@gmx.de>
2015-08-27 06:35:29 +0200

    msm: mdss: Adding lcd notifier
    Supports notifier for start&end of LCD power on&off
    
    Author and credits: Se Hun Kim <sean2.kim@lge.com>
    
    Conflicts:
    	drivers/video/msm/mdss/Makefile
    
    Change-Id: I40f49b607a3aa33f2e95ad55f25ca627419a22d8

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -21,6 +21,7 @@
 #include <linux/gpio.h>
 #include <linux/err.h>
 #include <linux/regulator/consumer.h>
+#include <linux/lcd_notify.h>
 
 #include "mdss.h"
 #include "mdss_panel.h"
@@ -864,6 +865,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 
 	switch (event) {
 	case MDSS_EVENT_UNBLANK:
+		lcd_notifier_call_chain(LCD_EVENT_ON_START, NULL);
 		rc = mdss_dsi_on(pdata);
 		mdss_dsi_op_mode_config(pdata->panel_info.mipi.mode,
 							pdata);
@@ -875,9 +877,11 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 		if (ctrl_pdata->on_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_unblank(pdata);
 		state_suspended = false;
+		lcd_notifier_call_chain(LCD_EVENT_ON_END, NULL);
 		break;
 	case MDSS_EVENT_BLANK:
 		power_state = (int) (unsigned long) arg;
+		lcd_notifier_call_chain(LCD_EVENT_OFF_START, NULL);
 		if (ctrl_pdata->off_cmds.link_state == DSI_HS_MODE)
 			rc = mdss_dsi_blank(pdata, power_state);
 		break;
@@ -888,6 +892,7 @@ static int mdss_dsi_event_handler(struct mdss_panel_data *pdata,
 			rc = mdss_dsi_blank(pdata, power_state);
 		rc = mdss_dsi_off(pdata, power_state);
 		state_suspended = true;
+		lcd_notifier_call_chain(LCD_EVENT_OFF_END, NULL);                
 #ifdef CONFIG_STATE_NOTIFIER
 		if (!use_fb_notifier)
 			state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
