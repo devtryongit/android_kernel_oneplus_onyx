7f29ea62929c0627977c68f124cb1dbb09230edb
CertifiedBlyndGuy <rjmurdok@linux.com>
2016-12-24 12:42:00 -0700

    Mark-XIII

--- deCAF.sh
+++ deCAF.sh
@@ -10,7 +10,7 @@
 # Parameters to be configured manually
 #######################################
 
-deCAF_VERSION="Mark-XII"
+deCAF_VERSION="Mark-XIII"
 
 TOOLCHAIN="/opt/toolchains/u6X/bin/arm-linux-androideabi-"
 ARCHITECTURE=arm
