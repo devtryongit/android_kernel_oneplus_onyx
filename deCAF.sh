b03d0264183497290d12fe23e50c5f2e072741dd
CertifiedBlyndGuy <rjmurdok@linux.com>
2016-12-30 20:51:19 -0700

    Mark-XXVIII

--- deCAF.sh
+++ deCAF.sh
@@ -10,7 +10,7 @@
 # Parameters to be configured manually
 #######################################
 
-deCAF_VERSION="Mark-XXVII"
+deCAF_VERSION="Mark-XXVIII"
 
 TOOLCHAIN="/opt/toolchains/u6X/bin/arm-linux-androideabi-"
 ARCHITECTURE=arm
