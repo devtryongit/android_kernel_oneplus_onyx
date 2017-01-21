260fc73355774bb49ec70df3aaca203ed6851f75
CertifiedBlyndGuy <rjmurdok@linux.com>
2017-01-20 20:41:34 -0700

    Mark-I
    Change-Id: Ibedbaf1c524e45ff73f3644229d5a28bb6ffbfcd

--- deCAF.sh
+++ deCAF.sh
@@ -10,7 +10,7 @@
 # Parameters to be configured manually
 #######################################
 
-deCAF_VERSION="Mark-"
+deCAF_VERSION="Mark-I"
 
 TOOLCHAIN="/opt/toolchains/u6X/bin/arm-eabi-"
 ARCHITECTURE=arm
