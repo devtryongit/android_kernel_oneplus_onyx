8a525abd1a7a80976eb1dc6a0966e127d5bfbff2
RJ Murdok <rjmurdok@linux.com>
2017-03-17 20:43:37 -0600

    msm: import of Alucard hotplug
    Signed-off-by: RJ Murdok <rjmurdok@linux.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -514,6 +514,17 @@ DECLARE_PER_CPU(struct rq, runqueues);
 #define cpu_curr(cpu)		(cpu_rq(cpu)->curr)
 #define raw_rq()		(&__raw_get_cpu_var(runqueues))
 
+#ifdef CONFIG_ALUCARD_HOTPLUG
+struct nr_stats_s {
+	/* time-based average load */
+	u64 nr_last_stamp;
+	unsigned int ave_nr_running;
+	seqcount_t ave_seqcnt;
+};
+
+DECLARE_PER_CPU(struct nr_stats_s, runqueue_stats);
+#endif
+
 #ifdef CONFIG_SMP
 
 #define rcu_dereference_check_sched_domain(p) \
@@ -987,10 +998,11 @@ static inline void cpuacct_charge(struct task_struct *tsk, u64 cputime) {}
 
 static inline unsigned int do_avg_nr_running(struct rq *rq)
 {
+	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
+	unsigned int ave_nr_running = nr_stats->ave_nr_running;
 	s64 nr, deltax;
-	unsigned int ave_nr_running = rq->ave_nr_running;
 
-	deltax = rq->clock_task - rq->nr_last_stamp;
+	deltax = rq->clock_task - nr_stats->nr_last_stamp;
 	nr = NR_AVE_SCALE(rq->nr_running);
 
 	if (deltax > NR_AVE_PERIOD)
@@ -1004,24 +1016,40 @@ static inline unsigned int do_avg_nr_running(struct rq *rq)
 
 static inline void inc_nr_running(struct rq *rq)
 {
-	write_seqcount_begin(&rq->ave_seqcnt);
-	rq->ave_nr_running = do_avg_nr_running(rq);
-	rq->nr_last_stamp = rq->clock_task;
-	rq->nr_running++;
+#ifdef CONFIG_ALUCARD_HOTPLUG
+	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
+#endif
 
-	if (rq->nr_running >= 2) {
+#ifdef CONFIG_ALUCARD_HOTPLUG
+	write_seqcount_begin(&nr_stats->ave_seqcnt);
+	nr_stats->ave_nr_running = do_avg_nr_running(rq);
+	nr_stats->nr_last_stamp = rq->clock_task;
+#endif
+	rq->nr_running++;
+    if (rq->nr_running >= 2) {
 		if (!rq->rd->overload)
 			rq->rd->overload = true;
 	}
+#ifdef CONFIG_ALUCARD_HOTPLUG
+	write_seqcount_end(&nr_stats->ave_seqcnt);
+#endif
 }
 
 static inline void dec_nr_running(struct rq *rq)
 {
-	write_seqcount_begin(&rq->ave_seqcnt);
-	rq->ave_nr_running = do_avg_nr_running(rq);
-	rq->nr_last_stamp = rq->clock_task;
+#ifdef CONFIG_ALUCARD_HOTPLUG
+	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
+#endif
+
+#ifdef CONFIG_ALUCARD_HOTPLUG
+	write_seqcount_begin(&nr_stats->ave_seqcnt);
+	nr_stats->ave_nr_running = do_avg_nr_running(rq);
+	nr_stats->nr_last_stamp = rq->clock_task;
+#endif
 	rq->nr_running--;
-	write_seqcount_end(&rq->ave_seqcnt);
+#ifdef CONFIG_ALUCARD_HOTPLUG
+	write_seqcount_end(&nr_stats->ave_seqcnt);
+#endif
 }
 
 extern void update_rq_clock(struct rq *rq);
